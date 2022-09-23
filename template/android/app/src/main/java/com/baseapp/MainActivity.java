e1c0b91d4e6ff1f43286edad4947c35c32d518c2
Nguyen Thanh Phuong <ccvn@CCVNs-Mac-mini-2.local>
2022-09-23 14:51:22 +0700

    Task: Upgrade to RN0.70.1

--- template/android/app/src/main/java/com/baseapp/MainActivity.java
+++ template/android/app/src/main/java/com/baseapp/MainActivity.java
@@ -9,6 +9,12 @@ import com.zoontek.rnbootsplash.RNBootSplash;
 
 public class MainActivity extends ReactActivity {
 
+  @Override
+    protected void onCreate(Bundle savedInstanceState) {
+      RNBootSplash.init(this); // <- initialize the splash screen
+      super.onCreate(null); // or super.onCreate(null) with react-native-screens
+    }
+
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
@@ -33,16 +39,6 @@ public class MainActivity extends ReactActivity {
       super(activity, mainComponentName);
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-      super.onCreate(null);
-    }
-
-    @Override
-    protected void loadApp(String appKey) {
-      RNBootSplash.init(getPlainActivity());
-      super.loadApp(appKey);
-    }
 
     @Override
     protected ReactRootView createRootView() {
