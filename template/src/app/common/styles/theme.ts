2a928b1c32993913271d989bcdfa49c279f45fa3
Nguyen Thanh Phuong <ccvn@CCVNs-Mac-mini-2.local>
2022-09-29 15:12:02 +0700

    Task: Add font for app

--- template/src/app/common/styles/theme.ts
+++ template/src/app/common/styles/theme.ts
@@ -1,10 +1,16 @@
 const commonColor = {
   modalBgColor: 'rgba(0,0,0,.3)',
+  info: '#ffd700',
+  error: 'rgb(255, 59, 48)',
 };
 export const myTheme = {
   primary: '#42CCC8',
   secondary: '#fff',
-  background: '#fff',
+  background: 'rgb(242, 242, 242)',
+  card: 'rgb(255, 255, 255)',
+  text: 'rgb(28, 28, 30)',
+  border: 'rgb(216, 216, 216)',
+  notification: 'rgb(255, 59, 48)',
   textColor: 'hsl(210,8%,25%)',
   ...commonColor,
 };
@@ -13,5 +19,9 @@ export const myThemeDark = {
   secondary: '#fff',
   textColor: 'hsl(210,8%,25%)',
   background: '#111',
+  card: 'rgb(18, 18, 18)',
+  text: 'rgb(229, 229, 231)',
+  border: 'rgb(39, 39, 41)',
+  notification: 'rgb(255, 69, 58)',
   ...commonColor,
 };
